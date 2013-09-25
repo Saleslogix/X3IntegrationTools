12ae9103144c177c299440dfa0846a4e444e2cc7
John Perona <jperona@gmail.com>
2013-09-25 10:58:25 -0700

    Initial Checkin of First Tool

---
+++ IntegrationAccountMergeUtility/AccountDataReassignment.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace IntegrationAccountMergeUtility
+{
+    partial class AccountDataReassignment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtAccountChanges = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbDelete = new System.Windows.Forms.CheckBox();
+            this.cbNewContact = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblStatus.Location = new System.Drawing.Point(12, 120);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(40, 13);
+            this.lblStatus.TabIndex = 13;
+            this.lblStatus.Text = "Status:";
+            // 
+            // label3
+            // 
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(269, 50);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Re-assignes all activity, history, and attachments from existing SalesLogix dupli" +
+    "cate account to imported integration account.";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnUpdate.Location = new System.Drawing.Point(180, 214);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 24);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Re-Assign";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtAccountChanges
+            // 
+            this.txtAccountChanges.Location = new System.Drawing.Point(155, 151);
+            this.txtAccountChanges.Name = "txtAccountChanges";
+            this.txtAccountChanges.Size = new System.Drawing.Size(100, 20);
+            this.txtAccountChanges.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(12, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Account records processed";
+            // 
+            // cbDelete
+            // 
+            this.cbDelete.AutoSize = true;
+            this.cbDelete.Checked = true;
+            this.cbDelete.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbDelete.Location = new System.Drawing.Point(15, 58);
+            this.cbDelete.Name = "cbDelete";
+            this.cbDelete.Size = new System.Drawing.Size(145, 17);
+            this.cbDelete.TabIndex = 14;
+            this.cbDelete.Text = "Delete duplicate account";
+            this.cbDelete.UseVisualStyleBackColor = true;
+            // 
+            // cbNewContact
+            // 
+            this.cbNewContact.AutoSize = true;
+            this.cbNewContact.Checked = true;
+            this.cbNewContact.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbNewContact.Location = new System.Drawing.Point(15, 81);
+            this.cbNewContact.Name = "cbNewContact";
+            this.cbNewContact.Size = new System.Drawing.Size(120, 17);
+            this.cbNewContact.TabIndex = 15;
+            this.cbNewContact.Text = "Move new contacts";
+            this.cbNewContact.UseVisualStyleBackColor = true;
+            // 
+            // AccountDataReassignment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 263);
+            this.Controls.Add(this.cbNewContact);
+            this.Controls.Add(this.cbDelete);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtAccountChanges);
+            this.Controls.Add(this.label1);
+            this.Name = "AccountDataReassignment";
+            this.Text = "Account Data Re-Assignment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtAccountChanges;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbDelete;
+        private System.Windows.Forms.CheckBox cbNewContact;
+    }
+}
+
---
+++ IntegrationAccountMergeUtility/AccountDataReassignment.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using log4net;
+using Sage.Platform.Data;
+using Sage.Platform.Orm;
+
+namespace IntegrationAccountMergeUtility
+{
+    public partial class AccountDataReassignment : Form
+    {
+        private static ConnectionStringDataService dataService;
+        private static readonly ILog logger = LogManager.GetLogger("SDataSync");
+
+        public AccountDataReassignment()
+        {
+            InitializeComponent();
+            SetupApplicationContext();
+        }
+
+        private static void SetupApplicationContext()
+        {
+            try
+            {
+                Sage.Platform.Application.ApplicationContext.Initialize("SalesLogix");
+
+                var conString = System.Configuration.ConfigurationManager.ConnectionStrings[0].ConnectionString;
+                dataService = new ConnectionStringDataService(conString);
+                var conn = dataService.GetConnection();
+                
+                conn.Open();
+                conn.Close();
+                Sage.Platform.Application.ApplicationContext.Current.Services.Add(typeof(IDataService), dataService);
+
+
+            }
+            catch (Exception e)
+            {
+                var message = e.Message;
+                if (e is OleDbException)
+                {
+                    message = string.Format(Properties.Resources.Error_Invalid_Connection, dataService.Server,
+                                            dataService.Database);
+                    logger.Error(message);
+                }
+                else
+                    logger.Error(e.Message, e);
+                throw new ApplicationException(message);
+            }
+        }
+
+        private void ExecuteCmd(IDbCommand cmd, string sql)
+        {
+            try
+            {
+                cmd.CommandText = sql;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                logger.ErrorFormat("AccountDataReassignment - Statement failed: {0}  ERROR: {1}", sql, e.Message);
+            }
+        }
+
+        private void MoveData(SessionScopeWrapper session, string source, string target)
+        {
+            var cmd = session.Connection.CreateCommand();
+            var cmd2 = session.Connection.CreateCommand();
+            var delCmd = session.Connection.CreateCommand();
+            try
+            {
+                if (cmd.Connection.State == ConnectionState.Closed)
+                    cmd.Connection.Open();
+                ExecuteCmd(cmd,string.Format("UPDATE ACTIVITY SET ACCOUNTID = '{0}' WHERE ACCOUNTID = '{1}'", target, source));
+                ExecuteCmd(cmd,cmd.CommandText = string.Format("UPDATE HISTORY SET ACCOUNTID = '{0}' WHERE ACCOUNTID = '{1}'", target, source));
+                ExecuteCmd(cmd,cmd.CommandText = string.Format("UPDATE ATTACHMENT SET ACCOUNTID = '{0}' WHERE ACCOUNTID = '{1}'", target, source));
+
+                cmd.CommandText = string.Format("SELECT CONTACTID, LASTNAME FROM CONTACT WHERE ACCOUNTID = '{0}'", target);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader != null && reader.Read())
+                    {
+                        try
+                        {
+
+                            cmd2.CommandText =
+                                string.Format(
+                                    "SELECT CONTACTID FROM CONTACT WHERE ACCOUNTID = '{0}' AND LASTNAME = '{1}'",
+                                    source, reader.GetString(1));
+
+                            using (var reader2 = cmd2.ExecuteReader())
+                            {
+                                while (reader2 != null && reader2.Read())
+                                {
+                                    MoveContactData(session, reader2.GetString(0), reader.GetString(0));
+                                    ExecuteCmd(delCmd,string.Format("delete from contact where contactid='{0}'", reader2.GetString(0)));
+                                }
+                            }
+                        }
+                        catch (Exception innerError)
+                        {
+                            logger.Error("Moving contact data to X3 contact: " +innerError);
+                        }
+                    }
+                }
+
+                if (cbNewContact.Checked)
+                {
+                    ExecuteCmd(cmd,string.Format("UPDATE CONTACT SET ACCOUNTID = '{0}' WHERE ACCOUNTID = '{1}'", target, source));
+
+                    //Assign UUIDs to new contacts that are not currently linked.
+                    ExecuteCmd(cmd,string.Format("UPDATE CONTACT SET GLOBALSYNCID = CONVERT(varchar(36), NEWID()) WHERE ACCOUNTID = '{0}' AND GLOBALSYNCID IS NULL", target));
+                }
+                else
+                {  // Contacts are not moved to X3 account, but are logged to a csv file to be reviewed.
+                    cmd.CommandText = string.Format("SELECT FIRSTNAME,LASTNAME,ACCOUNT,TITLE,WORKPHONE,HOMEPHONE,FAX,MOBILE,EMAIL FROM CONTACT WHERE ACCOUNTID = '{0}'", source);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        var sb = new StringBuilder();
+                        while (reader != null && reader.Read())
+                        {
+                            try
+                            {
+                                sb.AppendLine(string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8}", GetFieldAsString(reader, 0),
+                                                            GetFieldAsString(reader, 1), GetFieldAsString(reader, 2), GetFieldAsString(reader, 3),
+                                                            GetFieldAsString(reader, 4), GetFieldAsString(reader, 5), GetFieldAsString(reader, 6),
+                                                            GetFieldAsString(reader, 7), GetFieldAsString(reader, 8)));
+                            }
+                            catch (Exception innerError)
+                            {
+                                logger.Error("Logging New Contacts not moved: " + innerError);
+                            }
+                        }
+                        File.AppendAllText(@"c:\temp\Contacts Not Moved.csv", sb.ToString());
+                    }
+                }
+
+                if (cbDelete.Checked)
+                {
+                    ExecuteCmd(cmd,string.Format("DELETE FROM ACCOUNT WHERE ACCOUNTID = '{0}'", source));
+                }
+            }
+            catch (Exception error)
+            {
+                logger.Error("Moving account data: " + error);
+            }
+        }
+
+        private string GetFieldAsString(IDataReader reader, int index)
+        {
+            if (reader.IsDBNull(index)) return string.Empty;
+            return reader.GetString(index);
+        }
+
+        private void MoveContactData(SessionScopeWrapper session, string source, string target)
+        {
+            var cmd = session.Connection.CreateCommand();
+            try
+            {
+                if (cmd.Connection.State == ConnectionState.Closed)
+                    cmd.Connection.Open();
+                ExecuteCmd(cmd,string.Format("UPDATE ACTIVITY SET CONTACTID = '{0}' WHERE CONTACTID = '{1}'", target, source));
+                ExecuteCmd(cmd,string.Format("UPDATE HISTORY SET CONTACTID = '{0}' WHERE CONTACTID = '{1}'", target, source));
+                ExecuteCmd(cmd,string.Format("UPDATE ATTACHMENT SET CONTACTID = '{0}' WHERE CONTACTID = '{1}'", target, source));
+
+
+                if (cbDelete.Checked)
+                {
+                    ExecuteCmd(cmd,string.Format("DELETE FROM CONTACT WHERE CONTACTID = '{0}'", source));
+                }
+            }
+            catch (Exception error)
+            {
+                logger.Error(error);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = Properties.Resources.Status_New_Session;
+            Application.DoEvents();
+            using (var session = new SessionScopeWrapper())
+            {
+                using (var cmd = session.Connection.CreateCommand())
+                {
+                    lblStatus.Text = Properties.Resources.Status_Account_Loading;
+                    Application.DoEvents();
+                    var sql = "SELECT ACCOUNTID,ACCOUNT FROM SYSDBA.ACCOUNT WHERE GLOBALSYNCID IS NOT NULL";
+                    cmd.CommandText = sql;
+                    var count = 0;
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        using (var cmd2 = session.Connection.CreateCommand())
+                        {
+                            lblStatus.Text = Properties.Resources.Status_Account_Processing;
+                            Application.DoEvents();
+                            //foreach (IAccount account in accounts)
+                            while (reader != null && reader.Read())
+                            {
+                                count++;
+                                var status = 0;
+                                Math.DivRem(count, 3, out status);
+                                if (status == 0)
+                                {
+                                    txtAccountChanges.Text = count.ToString();
+                                    Application.DoEvents();
+                                }
+                                var target = reader.GetString(0);
+                                try
+                                {
+
+                                    cmd2.CommandText = string.Format("SELECT ACCOUNTID FROM ACCOUNT WHERE ACCOUNT = '{0}' and GLOBALSYNCID IS NULL",
+                                            reader.GetString(1));
+                                    using (var reader2 = cmd2.ExecuteReader())
+                                    {
+                                        var id = "";
+                                        var innerCount = 0;
+                                        while (reader2 != null && reader2.Read())
+                                        {
+                                            innerCount++;
+                                            if (innerCount == 1)
+                                            {id = reader2.GetString(0);}
+                                        }
+                                        if (innerCount == 1)
+                                        {MoveData(session, id, target);}
+                                    }
+                                }
+                                catch (Exception error)
+                                {
+                                    logger.Error(error);
+                                }
+                            }
+
+                            txtAccountChanges.Text = count.ToString();
+                            Application.DoEvents();
+                        }
+                    }
+
+
+                }
+
+            }
+        }
+
+    }
+}
